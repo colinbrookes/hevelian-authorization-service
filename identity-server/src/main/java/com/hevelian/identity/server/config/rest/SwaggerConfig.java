93ffb319991b0c81a6c0e6a02b563b48836098f6
Hevelian <colin@4tune.net>
2016-05-08 16:26:29 +0200

    fixed path mapping for swagger

--- identity-server/src/main/java/com/hevelian/identity/server/config/rest/SwaggerConfig.java
+++ identity-server/src/main/java/com/hevelian/identity/server/config/rest/SwaggerConfig.java
@@ -15,6 +15,6 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any()).build();
+                .paths(PathSelectors.any()).build().pathMapping("/api/");
     }
 }

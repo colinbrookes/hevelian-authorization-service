0106129c568ed0d4e613cab74d142d4efeffacbb
Yaroslav Shymkiv <jaroslav1h3best@gmail.com>
2018-09-26 15:18:49 +0300

    Merge branch 'master' of D:\Project\Hevelian-identity-server\hevelian-identity-server with conflicts.

--- core-api/src/main/java/com/hevelian/identity/core/api/TenantController.java
+++ core-api/src/main/java/com/hevelian/identity/core/api/TenantController.java
@@ -3,12 +3,15 @@ package com.hevelian.identity.core.api;
 import com.hevelian.identity.core.TenantService;
 import com.hevelian.identity.core.TenantService.TenantActiveAlreadyInStateException;
 import com.hevelian.identity.core.TenantService.TenantDomainAlreadyExistsException;
+import com.hevelian.identity.core.TenantService.TenantHasNoLogoException;
 import com.hevelian.identity.core.TenantService.TenantNotFoundByDomainException;
 import com.hevelian.identity.core.api.dto.TenantAdminRequestDTO.NewTenantGroup;
 import com.hevelian.identity.core.api.dto.TenantDomainDTO;
 import com.hevelian.identity.core.api.dto.TenantRequestDTO;
 import com.hevelian.identity.core.api.pagination.PageRequestParameters;
 import com.hevelian.identity.core.api.pagination.PageRequestParametersReader;
+import com.hevelian.identity.core.api.validation.constraints.Logo;
+import com.hevelian.identity.core.exc.ImageReadException;
 import com.hevelian.identity.core.model.Tenant;
 import com.hevelian.identity.core.pagination.PageRequestBuilder;
 import com.hevelian.identity.core.specification.EntitySpecificationsBuilder;
@@ -17,13 +20,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.groups.Default;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 import static com.hevelian.identity.core.model.Tenant.FIELD_DATE_CREATED;
@@ -35,6 +42,8 @@ import static com.hevelian.identity.core.specification.EntitySpecification.TO;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Validated
 public class TenantController {
+  private final String domain = "Tenant domain";
+  private final String tenantLogo = "Tenant logo. It is a 'image/png' or 'image/jpeg' file where width/height is not greater than 300 pixels";
   private final TenantService tenantService;
   private final PasswordEncoder passwordEncoder;
 
@@ -60,6 +69,24 @@ public class TenantController {
         tenantService.addTenant(tenant.toEntity(), tenant.getTenantAdmin().toEntity()).getDomain());
   }
 
+  @RequestMapping(path = "/setTenantLogo", method = RequestMethod.POST)
+  public void setTenantLogo(@ApiParam(value = domain, required = true) @RequestParam String tenantDomain,
+                            @ApiParam(value = tenantLogo, required = true) @Logo @RequestParam MultipartFile logo)
+      throws TenantNotFoundByDomainException, ImageReadException {
+    try {
+      tenantService.setTenantLogo(tenantDomain, logo.getBytes());
+    } catch (IOException e) {
+      throw new ImageReadException("Failed in converting MultipartFile to byte array.",e);
+    }
+  }
+
+  @RequestMapping(path = "/getTenantLogo", method = RequestMethod.GET,
+      produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
+  public BufferedImage getTenantLogo(@ApiParam(value = domain, required = true) @RequestParam String tenantDomain)
+      throws TenantNotFoundByDomainException, TenantHasNoLogoException {
+    return tenantService.getTenantLogo(tenantDomain);
+  }
+
   @RequestMapping(path = "/updateTenant", method = RequestMethod.POST)
   public void updateTenant(@Valid @RequestBody TenantRequestDTO tenant)
       throws TenantNotFoundByDomainException {
@@ -82,8 +109,8 @@ public class TenantController {
   }
 
   @RequestMapping(path = "/getAllTenants", method = RequestMethod.GET)
-  public Page<Tenant> getAllTenants(@ApiParam(value = PageRequestParameters.PAGE_DESCRIPTION) @RequestParam(name = PageRequestParameters.PAGE, required = false) @Min(PageRequestParameters.PAGE_MIN) Integer page,
-                                    @ApiParam(value = PageRequestParameters.SIZE_DESCRIPTION) @RequestParam(name = PageRequestParameters.SIZE, required = false) @Min(PageRequestParameters.SIZE_MIN) Integer size,
+  public Page<Tenant> getAllTenants(@ApiParam(value = PageRequestParameters.PAGE_DESCRIPTION,defaultValue = PageRequestParameters.DEFAULT_PAGE) @RequestParam(name = PageRequestParameters.PAGE, required = false) @Min(PageRequestParameters.PAGE_MIN) Integer page,
+                                    @ApiParam(value = PageRequestParameters.SIZE_DESCRIPTION,defaultValue = PageRequestParameters.DEFAULT_SIZE) @RequestParam(name = PageRequestParameters.SIZE, required = false) @Min(PageRequestParameters.SIZE_MIN) Integer size,
                                     @ApiParam(value = PageRequestParameters.SORT_DESCRIPTION) @RequestParam(name = PageRequestParameters.SORT, required = false) String sort,
                                     @ApiParam(value = "Domain") @RequestParam(required = false) String domain,
                                     @ApiParam(value = "Tenant is active") @RequestParam(required = false) Boolean active,
